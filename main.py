5b1f71874f731269bf2a9ae897d01836184d2938
nmanley03 <nmanley03@gmail.com>
2021-09-21 20:15:30 +0100

    Import player data csv.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+#import pandas
 import pandas as pd
 
-data = pd.read_csv(r"/Users/niallmanley/Downloads/Data_Viz/GBvideos.csv")
+#import csv file
+data = pd.read_csv(r"/Users/niallmanley/Downloads/Final_Project/202021Football.csv")
 
-print(data.head(50))
+# print heads and info on dataframe
+print(data.head())
+print(data.info())
+print(data.shape)
+
+#Count unique nationalities & clubs in the data set, and print.
+nationality_count = data['nationality'].nunique()
+club_count = data['Current Club'].nunique()
+print("This data set contains statistics for players from a total of " + str(club_count)+ " clubs across the Premier League & Championship in the 2020/21 season, from a total of " + str(nationality_count) + " countries.")
