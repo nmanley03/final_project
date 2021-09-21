861fac54bead5dd7303a9b85d92f2a1e8f1cdecb
nmanley03 <nmanley03@gmail.com>
2021-09-21 20:45:38 +0100

    Update player data csv.

--- main.py
+++ main.py
@@ -12,4 +12,6 @@ print(data.shape)
 #Count unique nationalities & clubs in the data set, and print.
 nationality_count = data['nationality'].nunique()
 club_count = data['Current Club'].nunique()
-print("This data set contains statistics for players from a total of " + str(club_count)+ " clubs across the Premier League & Championship in the 2020/21 season, from a total of " + str(nationality_count) + " countries.")
+league_count = data['league'].nunique()
+
+print("This data set contains statistics for players from a total of " + str(club_count)+ " clubs across the " +str(league_count)+ " professional English leagues in the 2020/21 season, who hail from a total of " + str(nationality_count) + " countries.")
